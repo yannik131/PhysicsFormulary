af72c3a261f4a465a7addf34fd8c5d9b046fea36
yannik131 <yannik131@web.de>
2019-08-15 22:28:58 +0200

    Added comments.

--- Variable.py
+++ Variable.py
@@ -37,6 +37,12 @@ class Constant(Variable):
         self.__locked = True
 
     def __setattr__(self, key, value):
+        """
+        Utilizes the __setattr__ method to make the constant immutable.
+        :param key: Name of the variable to change.
+        :param value: Value of the assignment.
+        :return: Nothing.
+        """
         if self.__dict__.get("_Constant__locked", False) and key == "value":
             raise AttributeError("Trying to assign to a constant!")
         self.__dict__[key] = value
