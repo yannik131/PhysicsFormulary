31f5a5a6d38e067368c10f12472e7c6989df98b0
yannik131 <yannik131@web.de>
2019-08-31 21:17:14 +0200

    Delete Options.py

--- Options.py
+++
@@ -1,8 +0,0 @@
-from Singleton import *
-import tkinter
-
-class Options(dict, metaclass=Singleton):
-
-    def addOption(self, t: tkinter.Variable, name: str, default_value):
-        self[name] = t()
-        self[name].set(default_value)
